06cb6f0ebbd221e11f169bfa6b3ed43483461350
Wojciech Szyjka <wojtus0708@gmail.com>
2022-10-27 17:39:45 +0200

    game construction

--- uni/Program.cs
+++ uni/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+
+namespace Tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] startBoard =
+            {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+            };
+            char[,] gameBoard = startBoard.Clone() as char[,];
+
+            bool player1Won = false;
+            bool player2Won = false;
+            bool nextIsPlayer1 = true;
+
+            for (int round = 0; round < gameBoard.Length; round++)
+            {
+                Console.Clear();
+
+                if (nextIsPlayer1)
+                {
+                    nextIsPlayer1 = false;
+                }
+                else
+                {
+                    nextIsPlayer1 = true;
+                }
+
+                if (player1Won || player2Won)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
